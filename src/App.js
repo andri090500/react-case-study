b12502f7b0c7781ba889bdae8cf3d80c99058c31
andri090500 <firmansyahandri.9500@gmail.com>
2023-03-08 14:09:33 +0700

    code case study front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Select from "react-select";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [isShow, setIsShow] = useState(false);
+
+  const getData = async () => {
+    try {
+      const res = await fetch("https://pokeapi.co/api/v2/berry");
+      const json = await res.json();
+      // 1. const result = json.results;
+      // 2. const result = json.results.map((data) => {
+      //   return data;
+      // });
+      /*3. */ const result = json.results.map((data) => {
+        return {
+          label: data.name,
+          value: data.name,
+        };
+      });
+      setDatas(result.sort((a, b) => a.label.localeCompare(b.label)));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleSubmit = () => {
+    setIsShow((curr) => !curr);
+  };
+
+  const handleChange = (e) => {
+    setSelected(e);
+    // setIsShow(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleSubmit()} disabled={!selected}>
+        {isShow ? "hide values" : "show values"}
+      </button>
+      <br />
+      <h2>{isShow ? selected : ""}</h2>
+      <br />
+      <Select options={datas} onChange={(e) => handleChange(e.value)} />
     </div>
   );
 }
